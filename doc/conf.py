6a3890351e313d051d85992c12280e4ff683f102
basejumpa <alexander.mannwahrenberg@gmail.com>
2024-01-24 20:13:52 +0100

    Change to theme Material

--- doc/conf.py
+++ doc/conf.py
@@ -1,1 +1,36 @@
+### SPHINX CONFIGURATION (GENERAL) ############################################
+# @see https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+### Project information #######################################################
+# @see https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 project="Personal Notebook"
+
+### General configuration #####################################################
+# @see https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Enable automatic numbering of figures
+numfig = True
+
+### Options for HTML output ###################################################
+# @see https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# @see https://bashtage.github.io/sphinx-material/customization.html
+
+html_theme = "sphinx_material"
+
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
+
+html_theme_options = {
+    "nav_title" : f"{project} Documentation",
+
+    "color_primary" : "teal",
+    "color_accent"  : "orange",
+
+    "globaltoc_depth"         : 3,
+    "globaltoc_collapse"      : "true",
+    "globaltoc_includehidden" : "true",
+}
+
+### EOF #######################################################################
